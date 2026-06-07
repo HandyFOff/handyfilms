fc8f272ce3491fa2cf8f8503c759b1bee2e11faf
HandyF <gordeysalnikov@gmail.com>
2026-06-07 12:14:57 +0300

    done architecture

---
+++ app/layout.tsx
@@ -0,0 +1,11 @@
+const RootLayout = ({children}: {children: React.ReactNode}) => {
+  return (
+    <html>
+      <body>
+        {children}
+      </body>
+    </html>
+  )
+}
+
+export default RootLayout;
